01479eee07771b4f92dc02b88a4286e623f527c3
Mitko Veta <mitko.veta@gmail.com>
2016-08-09 16:28:30 -0400

    added WSI processing example

---
+++ tepismat/examples/processWSI.m
@@ -0,0 +1,57 @@
+function P = processWSI(slide, level, blockSize, net, filterSize)
+% Example of block processing a whole slide image with a fully
+% convolutional neural network implemented in the Caffe deep learning
+% framework (http://caffe.berkeleyvision.org). The function returns a
+% probability map for an entire slide level.
+%
+% The entire probability map is kept in memory so this example is feasible
+% only for smaller layers. filterSize is the size of the patches used to
+% train the network (it is assumed that the FC network was obtained by
+% casting inner product layers to convolutional).
+%
+
+% for skipping mostly empty regions
+MAX_INTENSITY = 240;
+
+net.blobs('data').reshape([blockSize+filterSize blockSize+filterSize 3 1]);
+net.reshape();
+
+% row-column order
+levelSize = slide.PixelSize(level, [2 1]);
+
+% padding
+p = filterSize/2;
+
+P = zeros(levelSize, 'uint8');
+
+% ignore the border
+for row = 1:blockSize-filterSize:levelSize(1)-blockSize
+    for col = 1:blockSize-filterSize:levelSize(2)-blockSize
+        
+        disp([row col]);
+        
+        % OpenSlide returns 0s for out-of-region coordinates, no need to do
+        % "manual" padding
+        I = slide(row-p:row+blockSize-1+p, col-p:col+blockSize-1+p, level);
+        
+        % for testing purposes, returns the red channel
+        % P(row:row+regionSize-1, col:col+regionSize-1) = I(p+1:end-p,
+        % p+1:end-p, 1);
+        
+        % skip mostly empty regions
+        if mean(I(:)) > MAX_INTENSITY
+            P(row:row+blockSize-1, col:col+blockSize-1) = ...
+                zeros(blockSize, blockSize, 'uint8');
+        else
+            prob = net.forward({single(I(:,:,[3 2 1]))});
+            % label "1" is the target class
+            prob = prob{1}(:,:,2);
+            prob = uint8(255*prob);
+            P(row:row+blockSize-1, col:col+blockSize-1) = ...
+                imresize(prob, [blockSize blockSize]);
+        end
+        
+    end
+end
+
+end
